bf2a21043427a23001b2456d3d325cf0863115c5
mimar321 <mimar321@gmail.com>
2016-11-25 23:11:57 +0100

    Setup file and .gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pash',
+      version='0.1',
+      description='Run bash session from python',
+      url='http://github.com/mimar321/Pash',
+      author='Sinan Hepic',
+      author_email='sinan.hepic@gmail.com',
+      license='MIT',
+      packages=['pash'],
+      zip_safe=False)
+
