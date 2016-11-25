01ff26eee9bd770452a40086139e97e00a9112ce
mimar321 <mimar321@gmail.com>
2016-11-25 23:43:17 +0100

    Added test folder and updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='pash',
       version='0.1',
       description='Run bash session from python',
+      long_description=readme(),
       url='http://github.com/mimar321/Pash',
       author='Sinan Hepic',
       author_email='sinan.hepic@gmail.com',
